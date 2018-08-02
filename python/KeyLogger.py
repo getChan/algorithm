9196b1ee6e8276f2827e12dfe109679f68c8d8ec
getChan <9511chn@gmail.com>
2018-08-02 15:32:11 +0900

    리스트 삽입삭제 복잡도는 deque(이중연결리스트 기반) 사용하면 빠르다

---
+++ python/KeyLogger.py
@@ -0,0 +1,26 @@
+from sys import stdin
+read = stdin.readline
+from collections import deque
+
+n = int(read())
+
+for _ in range(n):
+    pw = read().rstrip()
+    output = deque()
+    cursor = 0
+    for i in pw:
+        if i == '<':
+            if cursor > 0:
+                cursor -= 1
+        elif i == '>': 
+            if cursor < len(output):
+                cursor += 1
+        elif i == '-':
+            if cursor > 0:
+                del output[cursor-1]
+                cursor -= 1
+        else:
+            output.insert(cursor, i)
+            cursor += 1
+    print(''.join(output))
+
