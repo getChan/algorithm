336da4e88d131f7ede5db6d79a95ea8090c958c2
getChan <9511chn@gmail.com>
2018-09-06 10:45:48 +0900

    File Move

---
+++ Dynamic_Programming/JLIS.py
@@ -0,0 +1,12 @@
+from sys import stdin
+read = stdin.readline
+
+c = int(read().rstrip())
+
+for _ in range(c):
+    n, m = (int(x) for x in read().rstrip())
+
+    a = [int(x) for x in read().rstrip()]
+    b = [int(x) for x in read().rstrip()]
+
+
