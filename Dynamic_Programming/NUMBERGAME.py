5a1785fa8c78fe85887197de014b4b5be3b470b8
getChan <201401438@inu.ac.kr>
2018-09-18 20:38:52 +0900

    Fail NUMBERGAME

--- Dynamic_Programming/NUMBERGAME.py
+++ Dynamic_Programming/NUMBERGAME.py
@@ -4,24 +4,31 @@ read = stdin.readline
 def memoization(func):
     from collections import defaultdict
     memo = defaultdict(dict)
-    def wrapper(i):
-        if i in memo:
-            if 
-            return memo[i]
-        memo[i] = func(i)
-        return memo[i]
+    def wrapper(a, b):
+        if a in memo:
+            if b in memo[a]:
+                return memo[a][b]
+        memo[a][b] = func(a,b)
+        return memo[a][b]
     return wrapper
+
 
 @memoization
 def play(left, right):
-    return max((numbers[left]-play(left+1, right)), numbers[right]-play(left, right-1),
-    -play(left+2, right), -play(left, right-2))
+    # 기저 사례
+    if left > right:
+        return 0
 
-
-
+    ret = max(numbers[left]-play(left+1, right), numbers[right]-play(left, right-1))
+    if right-left+1 >= 2:
+        ret = max(ret, -play(left+2, right))
+        ret = max(ret, -play(left, right-2))
+        
+    return ret
 
 c = int(read().rstrip())
 for _ in range(c):
     n = int(read().rstrip())
     numbers = [int(x) for x in read().rstrip().split()]
-
+    print(play(0, len(numbers)-1))
+    
